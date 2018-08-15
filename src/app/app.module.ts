21d51d22fea07aa0cb1d1ceee1b7ca1e73d009de
Ubuntu <barrybaugher@verizon.net>
2018-08-15 17:58:16 +0000

    main work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,34 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from './/material.module'
 import 'hammerjs'
+import { ManagerModule } from './manager/manager.module'
+import { InventoryModule } from './inventory/inventory.module'
+import { PosModule } from './pos/pos.module'
+import { UserModule } from './user/user.module'
+import { HomeComponent } from './home/home.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
+import { HttpClientModule } from '@angular/common/http'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ManagerModule,
+    InventoryModule,
+    PosModule,
+    UserModule,
+    HttpClientModule,
+    FlexLayoutModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 import { MatButtonModule, MatToolbarModule, MatIconModule } from '@angular/material'
 
 @NgModule({
-  imports: [
-    CommonModule, MatButtonModule, MatToolbarModule, MatIconModule
-  ],
-  exports: [
-    MatButtonModule, MatToolbarModule, MatIconModule
-  ],
-  declarations: []
+  imports: [CommonModule, MatButtonModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  declarations: [],
 })
-export class MaterialModule { }
+export class MaterialModule {}
