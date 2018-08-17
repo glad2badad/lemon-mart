24e31acd4ea64a7c75a2a3751f6c42cf48fa7590
Ubuntu <barrybaugher@verizon.net>
2018-08-17 11:57:48 +0000

    wrap up test changes

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
+// import { CommonModule } from '@angular/common'
 import { MatButtonModule, MatToolbarModule, MatIconModule } from '@angular/material'
 import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
   imports: [
-    CommonModule,
+    // CommonModule,
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
